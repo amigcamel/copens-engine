b5ca3a3f6aceb072d5d4eb227d10b1ba3581ec80
amigcamel <amigcamel@gmail.com>
2018-03-09 18:00:56 +0800

    microservice for cqpapi

---
+++ server.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""Micro-service for CQP API."""
+import json
+
+import falcon
+
+from cqpapi import Cqp
+
+
+class CWBResource:
+    """Resource for CWB."""
+
+    def on_get(self, req, resp):
+        """GET method."""
+        corpus_names = req.get_param_as_list('corpus_names', required=True)
+        token = req.get_param('token', required=True)
+        window_size = int(req.get_param('window_size', default=8))
+        rsize = int(req.get_param('rsize', default=0)) or None
+        show_pos = req.get_param_as_bool('show_pos', required=True)
+
+        conclst = []
+        for corpus_name in corpus_names:
+            cqp = Cqp(corpus_name=corpus_name,
+                      window_size=window_size)
+            cqp.find(token=token,
+                     rsize=rsize,
+                     show_pos=show_pos)
+            conclst += cqp.conclst
+
+        resp.body = json.dumps(conclst)
+        resp.content_type = falcon.MEDIA_JSON
+
+
+api = falcon.API()
+api.add_route('/cwb', CWBResource())
